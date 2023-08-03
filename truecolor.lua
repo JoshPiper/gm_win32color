6164c3313a8685139020402a7178e9125ad4c62a
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2023-08-03 19:08:40 +0100

    feat: Add the true color test script.

---
+++ truecolor.lua
@@ -0,0 +1,28 @@
+if not util.IsBinaryModuleInstalled("win32color") then
+    print("Binary Module Not Installed")
+    return
+end
+
+if not win32color then
+    require("win32color")
+end
+
+print("MsgC Output")
+local w, h = win32color.get_console_size()
+for i = 1, w do
+    MsgC(Color(math.Remap(i, 1, w, 0, 255), 0, 0), "|")
+end
+print()
+
+for i = 1, w do
+    MsgC(Color(0, math.Remap(i, 1, w, 0, 255), 0), "|")
+end
+print()
+
+for i = 1, w do
+    MsgC(Color(0, 0, math.Remap(i, 1, w, 0, 255)), "|")
+end
+print()
+
+print("ANSI Output")
+win32color.test()
